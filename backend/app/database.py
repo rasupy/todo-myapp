4e183f4e964a9acc8b7083aa098b85d62ec67f60
raspy <lyukey0902@gmail.com>
2025-09-12 16:23:36 +0900

    docs(backend): コードの整形

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,6 +1,8 @@
 import os
+from contextlib import contextmanager
+from typing import Generator
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 from models import Base
 import time
 from sqlalchemy.exc import OperationalError
@@ -9,6 +11,20 @@ from sqlalchemy.exc import OperationalError
 DB_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@db:5432/todo_db")
 engine = create_engine(DB_URL, echo=True)
 SessionLocal = sessionmaker(bind=engine)
+
+
+@contextmanager
+def session_scope() -> Generator[Session, None, None]:
+
+    session = SessionLocal()
+    try:
+        yield session
+        session.commit()
+    except Exception:
+        session.rollback()
+        raise
+    finally:
+        session.close()
 
 
 # テーブル作成（作成済みならスキップ）
