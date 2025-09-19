72445a9d50eb8c899e21d881fd659908a31561cd
raspy <lyukey0902@gmail.com>
2025-09-19 13:29:43 +0900

    feat(api): ログイン機能の実装

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -1,11 +1,13 @@
 from flask import Blueprint
 from .categories import categories_bp
 from .tasks import tasks_bp
+from .auth import auth_bp
 
 # 共通の Blueprint を作成
 api_bp = Blueprint("api", __name__)
 
 api_bp.register_blueprint(categories_bp)
 api_bp.register_blueprint(tasks_bp)
+api_bp.register_blueprint(auth_bp)
 
 __all__ = ["api_bp"]
